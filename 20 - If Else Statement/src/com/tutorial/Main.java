38a5e8d2277ed09ba791907f34dc0363e0fbbc59
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2020-11-08 15:41:02 +0700

    if else statement

---
+++ 20 - If Else Statement/src/com/tutorial/Main.java
@@ -0,0 +1,21 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // Tutorial untuk if statement atau percabangan
+
+        int a = 5;
+        System.out.println("nilai = " + 5);
+
+        // ini adalah cabangnya
+
+        if ( a == 5 ) {
+            System.out.println("ini adalah jalur true");
+        } else {
+            System.out.println("ini adalah jalur false");
+        }
+
+        System.out.println("selesai");
+    }
+}
