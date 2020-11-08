75ab1ffa9a405a632589f99bd798a339e788da3b
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2020-11-08 16:08:41 +0700

    if else if statement
    git
    git status

---
+++ 21 - If Else If Statement/src/com/tutorial/Main.java
@@ -0,0 +1,32 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // tutorial if else if statement
+
+            int a = 5;
+
+        System.out.println("ini adalah awal program");
+
+        // if else statement
+
+        if ( a == 5 ) {
+
+            System.out.println("ini adalah aksi 1");
+
+        } else if ( a == 10 ) {
+
+            System.out.println("ini adalah aksi 2");
+
+        } else {
+
+            System.out.println("ini adalah aksi default");
+
+        }
+
+        // akhir dari if else statement
+        System.out.println("ini adalah akhir program");
+
+    }
+}
