3e65faadc25d5a04278f568554f50cbe19a6ac2c
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2020-11-10 13:22:26 +0700

    switch case

---
+++ 23 - Switch Case/src/com/tutorial/Main.java
@@ -0,0 +1,35 @@
+package com.tutorial;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String input;
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("panggil nama: ");
+        input = inputUser.next();
+
+        // switch case
+
+        switch (input) {
+            case "otong":
+                System.out.println("saya otong dan hadir Bos!!!");
+                break;
+            case "ucup":
+                System.out.println("saya ucup dan hadir Bos!!!");
+                break;
+            case "mario":
+                System.out.println("saya mario dan hadir Bos!!!");
+                break;
+            default:
+                System.out.println(input + " tidak hadir Bos!!!");
+        }
+
+        System.out.println("program selesai");
+
+
+        // ekspresinya berupa satuan (int,long,byte,short), string, enum
+
+    }
+}
