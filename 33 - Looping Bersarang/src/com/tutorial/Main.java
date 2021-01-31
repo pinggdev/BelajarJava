05c242b0d7890028efd14ed98624491f79b582ec
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-01-31 22:41:03 +0700

    latihan fungsi

--- 33 - Looping Bersarang/src/com/tutorial/Main.java
+++ 33 - Looping Bersarang/src/com/tutorial/Main.java
@@ -85,5 +85,19 @@ public class Main {
             }
             System.out.println();
         }
+
+
+        System.out.println();
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i+(i-1); j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
 }
---
+++ 36 - Latihan Fungsi/src/com/tutorial/Main.java
@@ -0,0 +1,43 @@
+package com.tutorial;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("panjang = ");
+        int inputPanjang = userInput.nextInt();
+        System.out.print("lebar = ");
+        int inputLebar = userInput.nextInt();
+
+        gambar(inputPanjang, inputLebar);
+        tampilanKelilingDanLuas(inputPanjang, inputLebar);
+    }
+
+    private static void tampilanKelilingDanLuas(int panjang, int lebar) {
+        System.out.println("luas = " + luas(panjang, lebar));
+        System.out.println("keliling = " + keliling(panjang, lebar));
+    }
+
+    private static int keliling(int panjang, int lebar) {
+        int hasil = (panjang + lebar) * 2;
+        return hasil;
+    }
+
+    private static int luas(int panjang, int lebar) {
+        int hasil = panjang*lebar;
+        return hasil;
+    }
+
+    private static void gambar(int panjang, int lebar) {
+        for(int i = 0; i < lebar; i++) {
+            for (int j = 0; j < panjang; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
