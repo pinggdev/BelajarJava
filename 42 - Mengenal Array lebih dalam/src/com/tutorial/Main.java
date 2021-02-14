4d11a424dbb8b62ec6556f35aa0694360450c3d9
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-02-14 21:14:28 +0700

    mengenal array lebih dalam

---
+++ 42 - Mengenal Array lebih dalam/src/com/tutorial/Main.java
@@ -0,0 +1,45 @@
+package com.tutorial;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] arrayAngka1 = {1,2,3,4,5};
+        int[] arrayAngka2 = new int[5];
+
+        System.out.println(arrayAngka1);
+        System.out.println(arrayAngka2);
+
+        arrayAngka2 = arrayAngka1; // memberikan reference nya, bukan memberikan nilainya, atau bisa di bilang
+        // memberikan alamatnya
+
+        System.out.println(arrayAngka1);
+        System.out.println(arrayAngka2);
+
+        System.out.println("array 1 -> " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 -> " + Arrays.toString(arrayAngka2));
+
+        arrayAngka1[0] = 100;
+        arrayAngka2[4] = 400;
+        System.out.println("array 1 - > " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 - > " + Arrays.toString(arrayAngka2));
+
+        ubahArray(arrayAngka1);
+        System.out.println("array 1 -> " + Arrays.toString(arrayAngka1));
+        System.out.println("array 2 -> " + Arrays.toString(arrayAngka2));
+    }
+
+    // method yang argumentnya adalah reference, pass by reference
+    // bukan pass by value
+    private static void ubahArray(int[] dataArray) {
+        for(int data : dataArray) {
+            System.out.println(data);
+        }
+        dataArray[0] = 125;
+        for(int data : dataArray) {
+            System.out.println(data);
+        }
+    }
+}
